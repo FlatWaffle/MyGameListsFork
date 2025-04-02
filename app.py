6bc22df58799261a5416856e4e41e19d50cdddd5
ByteOfWaffle <156892485+ByteOfWaffle@users.noreply.github.com>
2025-04-02 10:54:08 +0200

    changed the order of the sql connection and the flask host, so that it inserts table first,
    previously it only inserted table when i ended it

--- app.py
+++ app.py
@@ -1,14 +1,6 @@
 from flask import Flask, render_template
 import pymysql
-app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Hello World!'
-
-if __name__ == '__main__':
-    app.run(debug=True)
-    
 conn = pymysql.connect(
     host='10.2.3.90', 
     user='root', 
@@ -20,7 +12,7 @@ cursor = conn.cursor()
 
 # SQL query to create the table
 sql_query = """
-CREATE TABLE IF NOT EXISTS testnumthree (
+CREATE TABLE IF NOT EXISTS testnum4 (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
     password VARCHAR(255) NOT NULL
@@ -33,3 +25,14 @@ cursor.execute(sql_query)
 # Commit changes and close the connection
 cursor.close()
 conn.close()
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
+
