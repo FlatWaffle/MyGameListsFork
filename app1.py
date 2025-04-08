9192c3b40fdeb06bdf5713872d608afc259b98ee
ByteOfWaffle <156892485+ByteOfWaffle@users.noreply.github.com>
2025-04-08 14:01:20 +0200

    Am trying to use waitress, also made gamedetails page
    Game details page currently only contains cyberpunk and a placeholder image, but will make it contain whatever the user presses on later.

--- app1.py
+++ app1.py
@@ -12,7 +12,7 @@ cursor = conn.cursor()
 
 # SQL query to create the table
 sql_query = """
-CREATE TABLE IF NOT EXISTS testnum4 (
+CREATE TABLE IF NOT EXISTS skibiditest (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
     password VARCHAR(255) NOT NULL
@@ -25,14 +25,5 @@ cursor.execute(sql_query)
 # Commit changes and close the connection
 cursor.close()
 conn.close()
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return 'Hello World!'
-
-if __name__ == '__main__':
-    app.run(debug=True)
     
 
