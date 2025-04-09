6eae458ba3b53156be536b808878d14aaa40ae97
ByteOfWaffle <156892485+ByteOfWaffle@users.noreply.github.com>
2025-04-09 09:50:59 +0200

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -2,9 +2,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const API_KEY = '6b9808e92cf04d038d16926869d56bad'; //Will be hidden later
     const BASE_URL = `https://api.rawg.io/api/games?key=${API_KEY}`;
     const gridContainer = document.getElementById('imageGrid');
-    let CurrentPage = 1;
-    function fetchGameData(page) {
-        fetch(`${BASE_URL}&page=${page}&page_size=20`) // Fetch games from API and get page number from current page.
+
+    function fetchGameData() {
+        fetch(`${BASE_URL}&page=1&page_size=20`) // Fetch games from API and get page number from current page.
             .then(response => response.json()) // Convert response to JSON.
             .then(data => {
                 // Loop through games in api response
@@ -14,13 +14,6 @@ document.addEventListener('DOMContentLoaded', () => {
                 });
             });
     }
-    //Load first page of games
-    fetchGameData(CurrentPage);
 
-    //Load more pages
-    loadMoreBtn.addEventListener('click', function() {
-        CurrentPage++; // Increases currentpage by 1
-        fetchGameData(CurrentPage);
-    });
     
 });
