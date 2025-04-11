d8e12373dd34a3af9d8ac0b0a681bc25435c0586
ByteOfWaffle <156892485+ByteOfWaffle@users.noreply.github.com>
2025-04-11 09:40:03 +0200

    GD now updates to correct image based in gameid

--- static/js/gameDetails.js
+++ static/js/gameDetails.js
@@ -1,10 +1,24 @@
 //Here I will fech the game id from local storage and use it to fetch the game details from the api.
 document.addEventListener('DOMContentLoaded', () => {
     const API_KEY = '6b9808e92cf04d038d16926869d56bad'; //Will be hidden later
-    const BASE_URL = `https://api.rawg.io/api/games?key=${API_KEY}`;
     const gameId = localStorage.getItem('SelectedGameId');
+    const BASE_URL = `https://api.rawg.io/api/games/${gameId}?key=${API_KEY}`;
     alert(gameId);
-    let image = document.getElementById("GDimage")
-    image.src = "https://cdn.pixabay.com/photo/2016/07/07/16/46/dice-1502706_640.jpg"
+    fetch(BASE_URL)
+
+    function fetchGameDetails() {
+        fetch(BASE_URL)
+            .then(response => response.json())
+            .then(game => {
+                // Get the image element
+                let image = document.getElementById("GDimage");
+                // Set the image src to the games image from api
+                image.src = game.background_image;
+            })
+    }
+
+    fetchGameDetails();
+
+
 
 });
