f0b811491f688d39a96fb2f22520e9ed8c99c6e4
ByteOfWaffle <156892485+ByteOfWaffle@users.noreply.github.com>
2025-05-16 09:07:43 +0200

    Changed to localhost for ease of use especially for others

--- waitress_server.py
+++ waitress_server.py
@@ -3,5 +3,5 @@ from app1 import app
 
 if __name__ == '__main__':
     print("Starting Waitress server...")
-    print("Access the application at http://10.2.2.104:8080")
+    print("Access the application at http://localhost:8080")
     serve(app, host='0.0.0.0', port=8080, url_scheme='http')
